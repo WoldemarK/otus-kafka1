01a3e8ee12bb64bbfd221a9e102ed4228c627383
Vladimir <kovtynov.vladimir@gmail.com>
2024-04-07 02:17:03 +0300

    Настройка проекта

---
+++ src/main/java/com/example/otuskafka1/OtusKafka1Application.java
@@ -0,0 +1,13 @@
+package com.example.otuskafka1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusKafka1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtusKafka1Application.class, args);
+    }
+
+}
